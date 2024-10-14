65e65c3a9f926bf46b8f5329445d76f17d974aaa
Valentstage2024 <Valentstage2024@gmail.com>
2024-10-14 16:31:57 +0200

    Update newsfeed.php

--- php/newsfeed.php
+++ php/newsfeed.php
@@ -14,21 +14,46 @@
     <?php include_once '../includes/header.php'; ?>
 
 
-    <main>
+  
+<main>
         <section class="newsletter-section">
             <h2>Nieuwsbrief</h2>
             <p class="intro-text">
-                Welkom bij de nieuwsbrief van de Elhouri Foundation! Hier vind je de laatste updates en belangrijke informatie over onze projecten en evenementen.
+                Welkom bij de nieuwsbrief van de El Houari Foundation! Hier vind je de laatste updates en belangrijke informatie over onze projecten en evenementen.
             </p>
+            
+            
             <div class="news-feed" id="news-feed">
-                <!-- Nieuwsitem 1 -->
-                <div class="news-item">
-                    <a href="news1.php">
-                        <img src="../img/voetbal.png" class="news-img">
-                    </a>
-                    <h3>Arme Gezinnen Helpen in Marokko</h3>
-                    <p>Samen met Dar Al Ousra bieden we ondersteuning aan kwetsbare gezinnen in Marokko.</p>
-                </div>
+            <div class="news-feed" id="news-feed">
+    <!-- Nieuwsitem 1 -->
+    <div class="news-item">
+        <a href="news1.php">
+            <img src="../img/voetbal.png" class="news-img" alt="Afbeelding van voetbalproject">
+        </a>
+        <h3>Arme Gezinnen Helpen in Marokko</h3>
+        <p>Samen met Dar Al Ousra bieden we ondersteuning aan kwetsbare gezinnen in Marokko.</p>
+    </div>
+
+    <!-- Nieuwsitem 2 -->
+    <div class="news-item">
+        <a href="news2.php">
+            <img src="../img/voetbal.png" class="news-img" alt="Afbeelding van educatief project">
+        </a>
+        <h3>Project Voor Educatie</h3>
+        <p>We zijn bezig met een nieuw educatief project voor kinderen in Marokko.</p>
+    </div>
+
+    <!-- Nieuwsitem 3 -->
+    <div class="news-item">
+        <a href="news3.php">
+            <img src="../img/voetbal.png" class="news-img" alt="Afbeelding van voedselhulp">
+        </a>
+        <h3>Voedselhulp in de Winter</h3>
+        <p>Onze organisatie verstrekt voedselpakketten aan behoeftigen in de winterperiode.</p>
+    </div>
+</div>
+
+
 
              
 
