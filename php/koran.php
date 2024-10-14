bf63cbf1b8b405a94d958a9cee6c6a9a02d524dc
Valentstage2024 <Valentstage2024@gmail.com>
2024-10-14 09:44:57 +0200

    mikail

--- php/koran.php
+++ php/koran.php
@@ -13,12 +13,87 @@
 
     <?php include_once '../includes/header.php'; ?>
 
+   
+     <div class="image-wrapper">
+    <img src="https://koran.nl/wp-content/plugins/quran-learning-app/public/app/assets/images/basmala.svg" alt="Beschrijving van de afbeelding">
+</div>
+   </di>
 
 
-    <section class="koran">
-        <h2>Koran</h2>
-        <div class="koran-container">
-            <h3>Surah Al-Fatiha</h3>
+    
+    <div class="sura-container">
+    <h2>Soera's</h2>
+    
+    <div class="sura">
+        
+        <h3>Soera Al-Fatiha (Soera 1)</h3>
+        <p lang="ar" dir="rtl">الْعَالَمِينَ الرَّحْمَـٰنِ الرَّحِيمِ مَالِكِ يَوْمِ الدِّينِ إِيَّاكَ نَعْبُدُ وَإِيَّاكَ نَسْتَعِينُ اهْدِنَا الصِّرَاطَ الْمُسْتَقِيمَ صِرَاطَ الَّذِينَ أَنْعَمْتَ عَلَيْهِمْ غَيْرِ الْمَغْضُوبِ عَلَيْهِمْ وَلَا الضَّالِّينَ</p>
+        <p>In de naam van Allah, de meest Barmhartige, de meest Genadevolle.</p> 
+        <p>Alle lof en dank komt (alléén) Allah toe, de Bezitter van (alle) wereldwezens (mensen, Djinn, Engelen en de rest van Zijn volledige schepping).</p>
+        <p>De meest Barmhartige (voor elk wezen in dit leven), de meest Genadevolle (voor Zijn gelovigen in het Hiernamaals).</p>
+        <p>De Heerser op de Dag des Oordeels.</p>
+        <p>U (alléén) aanbidden wij, en U (alléén) vragen wij om hulp.</p>
+        <p>Leidt ons op het rechte Pad.</p>
+        <p>Het Pad van degenen aan wie U gunsten hebt geschonken (verwijzend naar de Profeten en meer specifiek Mohammed) en niet (naar het pad) van degenen die Uw toorn hebben opgewekt (degenen die de Waarheid verwerpen of verbergen) noch (naar het pad) van de dwalenden.</p>
+        <audio controls>
+                <source src="../soera/001.mp3" type="audio/mpeg">
+                Je browser ondersteunt deze audio niet.
+            </audio>
+    </div>
+
+    <div class="sura">
+        <h3>Soera Al-Ikhlas (Soera 112)</h3>
+        <p lang="ar" dir="rtl">قُلۡ هُوَ ٱللَّهُ أَحَدٌ ٱللَّهُ ٱلصَّمَدُ لَمۡ يَلِدۡ وَلَمۡ يُولَدۡ</p>
+        <p>Zeg: Hij is Allah, de Enige.</p>
+        <p>Allah is Zichzelf- genoeg, Eeuwig.</p>
+        <p>Hij verwekte niet, noch werd Hij verwekt.</p>
+        <p>En niemand is Hem in enig opzicht gelijk.</p>
+        <audio controls>
+                <source src="../soera/112.mp3" type="audio/mpeg">
+                Je browser ondersteunt deze audio niet.
+            </audio>
+
+    
+    </div>
+
+    <div class="sura">
+        <h3>Soera Al-Falaq (Soera 113)</h3>
+        <p lang="ar" dir="rtl">قُلۡ أَعُوذُ بِرَبِّ ٱلۡفَلَقِمِن شَرِّ مَا خَلَقَوَمِن شَرِّ غَاسِقٍ إِذَا وَقَبَوَمِن شَرِّ ٱلنَّفَّٰثَٰتِ فِي ٱلۡعُقَدِوَمِن شَرِّ حَاسِدٍ إِذَا حَسَدَ</p>
+        <p>Zeg: “Ik zoek mijn toevlucht tot de Heer van de dageraad.</p>
+        <p>Tegen het kwaad van wat Hij heeft geschapen.</p>
+        <p>En tegen het kwaad van de duisternis wanneer deze zich verspreid.</p>
+        <p>En tegen het kwaad van degenendie op knopen blazen (de magiers).</p>
+        <p>En tegen het kwaad van jaloerse wanneer deze jaloers is.</p>
+        <audio controls>
+                <source src="../soera/113.mp3" type="audio/mpeg">
+                Je browser ondersteunt deze audio niet.
+            </audio>
+
+
+   
+    </div>
+
+    <div class="sura">
+        <h3>Soera An-Nas (Soera 114)</h3>
+        <p lang="ar" dir="rtl">قُلۡ أَعُوذُ بِرَبِّ ٱلنَّاسِمَلِكِ ٱلنَّاسِإِلَٰهِ ٱلنَّاسِمِن شَرِّ ٱلۡوَسۡوَاسِ ٱلۡخَنَّاسِٱلَّذِي يُوَسۡوِسُ فِي صُدُورِ ٱلنَّاسِمِنَ ٱلۡجِنَّةِ وَٱلنَّاسِ</p>
+        <p>Zeg: Ik zoek toevlucht bij de Heer van de mensen.</p>
+        <p>De Koning der mensen.</p>
+        <p>De God der mensen.</p>
+        <p>Tegen het kwaad van de wegsluipende fluisteraar ( d.w.z. Sjaitaan, deze trekt zich terug zodra Allah wordt herdacht).</p>
+        <p>Degene die in de harten van de mensheid fluistert.</p>
+        <p>Vanuit het midden der Djinn en mensen.</p>
+        <audio controls>
+                <source src="../soera/114.mp3" type="audio/mpeg">
+                Je browser ondersteunt deze audio niet.
+            </audio>
+
+
+    </div>
+</div>
+
+
+
+   
 
     <?php include_once '../includes/footer.php'; ?>
 
