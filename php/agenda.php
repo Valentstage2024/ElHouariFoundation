5e9bc84f8c10f09f389d27076a9aaf93e6c65b2b
Valentstage2024 <Valentstage2024@gmail.com>
2024-10-24 09:48:08 +0200

    mmmm

--- php/agenda.php
+++ php/agenda.php
@@ -45,6 +45,7 @@ foreach ($events as $index => $event) {
     </script>
     <script src="../js/eventNavigation.js"></script>
 </head>
+<div id="verse-display"></div>
 
 <body class="agenda-body">
 
@@ -85,37 +86,37 @@ foreach ($events as $index => $event) {
     <div class="calendar" id="calendar-app">
   <div class="calendar--day-view" id="day-view">
     <span class="day-view-exit" id="day-view-exit">&times;</span>
-    <span class="day-view-date" id="day-view-date">MAY 29 2016</span>
+    <span class="day-view-date" id="day-view-date">29 MEI 2016</span>
     <div class="day-view-content">
       <div class="day-highlight">
-        You <span class="day-events" id="day-events">had no events for today</span>. &nbsp; <span tabindex="0" onkeyup="if(event.keyCode != 13) return; this.click();" class="day-events-link" id="add-event" data-date>Add a new event?</span>
+        Je <span class="day-events" id="day-events">had geen evenementen voor vandaag</span>. &nbsp; <span tabindex="0" onkeyup="if(event.keyCode != 13) return; this.click();" class="day-events-link" id="add-event" data-date>Wil je een nieuw evenement toevoegen?</span>
       </div>
       <div class="day-add-event" id="add-day-event-box" data-active="false">
         <div class="row">
           <div class="half">
             <label class="add-event-label">
-               Name of event
-              <input type="text" class="add-event-edit add-event-edit--long" placeholder="New event" id="input-add-event-name">
+               Naam van het evenement
+              <input type="text" class="add-event-edit add-event-edit--long" placeholder="Nieuw evenement" id="input-add-event-name">
              
             </label>
           </div>
           <div class="qtr">
             <label class="add-event-label">
-          Start Time
+          Starttijd
               <input type="text" class="add-event-edit" placeholder="8:15" id="input-add-event-start-time" data-options="1,2,3,4,5,6,7,8,9,10,11,12" data-format="datetime">
               <input type="text" class="add-event-edit" placeholder="am" id="input-add-event-start-ampm" data-options="a,p,am,pm">
             </label>
           </div>
           <div class="qtr">
             <label class="add-event-label">
-          End Time
+          Eindtijd
               <input type="text" class="add-event-edit" placeholder="9" id="input-add-event-end-time" data-options="1,2,3,4,5,6,7,8,9,10,11,12" data-format="datetime">
               <input type="text" class="add-event-edit" placeholder="am" id="input-add-event-end-ampm" data-options="a,p,am,pm">
             </label>
           </div>
           <div class="half">
-            <a onkeyup="if(event.keyCode != 13) return; this.click();" tabindex="0" id="add-event-save" class="event-btn--save event-btn">save</a>
-            <a tabindex="0" id="add-event-cancel" class="event-btn--cancel event-btn">cancel</a>
+            <a onkeyup="if(event.keyCode != 13) return; this.click();" tabindex="0" id="add-event-save" class="event-btn--save event-btn">opslaan</a>
+            <a tabindex="0" id="add-event-cancel" class="event-btn--cancel event-btn">annuleren</a>
           </div>
         </div>
         
@@ -124,23 +125,23 @@ foreach ($events as $index => $event) {
         
       </div>
       <div class="day-inspiration-quote" id="inspirational-quote">
-        Every child is an artist.  The problem is how to remain an artist once he grows up. –Pablo Picasso
+        Elk kind is een kunstenaar. Het probleem is hoe kunstenaar te blijven als hij volwassen wordt. –Pablo Picasso
       </div>
     </div>
   </div>
   <div class="calendar--view" id="calendar-view">
     <div class="cview__month">
       <span class="cview__month-last" id="calendar-month-last">Apr</span>
-      <span class="cview__month-current" id="calendar-month">May</span>
+      <span class="cview__month-current" id="calendar-month">Mei</span>
       <span class="cview__month-next" id="calendar-month-next">Jun</span>
     </div>
-    <div class="cview__header">Sun</div>
-    <div class="cview__header">Mon</div>
-    <div class="cview__header">Tue</div>
-    <div class="cview__header">Wed</div>
-    <div class="cview__header">Thu</div>
-    <div class="cview__header">Fri</div>
-    <div class="cview__header">Sat</div>
+    <div class="cview__header">Zon</div>
+    <div class="cview__header">Maa</div>
+    <div class="cview__header">Din</div>
+    <div class="cview__header">Woe</div>
+    <div class="cview__header">Don</div>
+    <div class="cview__header">Vri</div>
+    <div class="cview__header">Zat</div>
     <div class="calendar--view" id="dates">
     </div>
   </div>
@@ -150,7 +151,8 @@ foreach ($events as $index => $event) {
   <div class="footer">
     
     
-    <span><span id="footer-date" class="footer__link">Today is May 30</span></span>    
+    <span><span id="footer-date" class="footer__link">Vandaag is 30 mei</span></span>
+
   </div>
 </div>
 </div>
